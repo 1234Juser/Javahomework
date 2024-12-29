2aab394639bd8b2e2d9e242c9f1103fde8916db0
user <1234Juser@gmail.com>
2024-12-29 20:38:14 +0900

    2

--- src/main/java/org/example/object06/hard/Singleton.java
+++ src/main/java/org/example/object06/hard/Singleton.java
@@ -1,24 +1,24 @@
 package org.example.object06.hard;
 
-//public class Singleton {
-//    /* Q2. 다음 조건에 맞는 클래스를 작성하고, 인스턴스를 생성하여 필드를 출력하세요.
-//     *
-//     * 복습 포인트:
-//     * - 싱글톤 패턴에 대해 이해할 수 있다.
-//     * - 싱글톤 패턴의 장단점에 대해 이해할 수 있다.
-//     * - 이른 초기화와 게으른 초기화를 구분하여 싱글톤 패턴을 구현할 수 있다.
-//     *
-//     * 클래스명: Singleton
-//     * 메소드: getInstance() - 싱글톤 객체를 반환하는 메소드
-//     *
-//     * 인스턴스 생성: Singleton 객체를 생성하고 메소드를 호출하여 같은 객체인지 확인
-//     *
-//     * 출력 예시:
-//     * 두 객체는 같은 인스턴스입니다.
-//     * */
-//    private static Singleton singleton = new Singleton();
-//    private Singleton(){};
-//    public static Singleton getInstance(){
-//        return instance;
-//    }
-//}
+public class Singleton {
+    /* Q2. 다음 조건에 맞는 클래스를 작성하고, 인스턴스를 생성하여 필드를 출력하세요.
+     *
+     * 복습 포인트:
+     * - 싱글톤 패턴에 대해 이해할 수 있다.
+     * - 싱글톤 패턴의 장단점에 대해 이해할 수 있다.
+     * - 이른 초기화와 게으른 초기화를 구분하여 싱글톤 패턴을 구현할 수 있다.
+     *
+     * 클래스명: Singleton
+     * 메소드: getInstance() - 싱글톤 객체를 반환하는 메소드
+     *
+     * 인스턴스 생성: Singleton 객체를 생성하고 메소드를 호출하여 같은 객체인지 확인
+     *
+     * 출력 예시:
+     * 두 객체는 같은 인스턴스입니다.
+     * */
+    private static Singleton singleton = new Singleton();
+    private Singleton(){};
+    public static Singleton getInstance(){
+        return instance;
+    }
+}
